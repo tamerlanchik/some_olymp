36b49c92d6bdd8695322be51046c438f4513f7bf
Andrey K <andreykochnov@yandex.ru>
2016-12-30 00:07:28 +0300

    Update C.py

--- C.py
+++ C.py
@@ -10,33 +10,41 @@ pars=[]
 for i in range(1, n+1):
     for j in range(i, n+1):
         if a[i]*a[j]!=0 and a[i]!= a[j]: pars.append((i, j))
-'''print("pars: ", pars)'''
-'''for i in F:
-    print(*i)'''
+print("pars: ", pars)
+for i in F:
+    print(*i)
 lens=[]
 visited=[[0]*(n+1) for j in range(n+1)]
 def dfc(i, j):
-    '''print("Hello: ", i, j)'''
-    if i==j: return True
+    print("Hello: ", i, j)
+    if i==j:
+        return 0
     for p in range(1, n+1):
-        #print(p)
+        print(p)
         if F[i][p]==1 and visited[i][p]==0:
             #print(i, p)
             visited[i][p]=visited[p][i]=1
-            '''for l in visited:
-                print(*l)'''
-            if dfc(p, j)==True:
+            for l in visited:
+                print(*l)
+            ans=dcf(p, j)
+            if ans!=-1:
+                visited[i][p]=visited[p][i]=0
+                
                 lens[-1]+=1
                 return True
     else:
-        return False
+        print("Break way", (i, j))
+        return -1
 for i in pars:
     l=0    
     lens.append(0)
     visited=[[0]*(n+1) for j in range(n+1)]
-    #print("Start: ", i)
+    print("Start: ", i)
     dfc(i[0], i[1])
+    print("Finish: ", lens[-1])
+    #if lens[-1]==0: lens.pop()
 print(min(lens))
+print(lens)
 
         
             
