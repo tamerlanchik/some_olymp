8b08b19cad4143fdab0c44ed674d9e6ad12bf69d
Andrey K <andreykochnov@yandex.ru>
2016-12-30 14:12:42 +0300

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-inline int mini(int a, int b, int c)
+inline int mini(unsigned long long int a, unsigned long long int b, unsigned long long int c)
 {
 	if (a>b)
 	{
@@ -20,21 +20,27 @@ int main() {
 	int n;
 	cin>>a>>b>>c;
 	cin>>n;
-	int *F=new int [n+1];
-	F[0] = 0;
+	int **F=new int* [29];
+	for(int i=0; i<=n; i++)
+        F[i]=new int;
+	*F[0] = 0;
 	int counter=0;
 	for (int i=1; i<=n; i++)
 	{
-		x=F[max(0, i-28)-counter];
-		y=F[max(0, i-7)-counter];
-		z=F[i-1-counter];
-		F[i-counter]=mini(x+c, y+b, z+a);
-		if (i-counter>28)
+        if (i>29)
+
+		x=*F[max(0, i-28)];
+		y=*F[max(0, i-7)];
+		z=*F[i-1];
+		*F[i]=mini(x+c, y+b, z+a);
+		if (i-counter>29)
 		{
-            F+=1;
+            delete F[counter];
             counter+=1;
 		}
     }
-    cout<<"Ans: "<<F[n-counter]<<endl;
+    cout<<*F[n];
+    cout<<endl<<counter;
+    cin>>x;
 	return 0;
 }
