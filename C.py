67f48fdcce8b017615a4d104c479d2881c06fc67
Andrey K <andreykochnov@yandex.ru>
2016-12-30 16:51:31 +0300

    Update C.py

--- C.py
+++ C.py
@@ -15,54 +15,85 @@ for i in range(1, n+1):
 #    print(*i)
 lens=[]
 visited=[0]*(n+1)
+distanses=[[-1]*(n+1) for i in range(n+1)]
+for k in range(n+1):
+    distanses[k][k]=0
 def dfc(i, j):
-    #print("Hello: ", i, j)
+    print("Hello: ", i, j)
+    if distanses[i][j]>=0:
+        print("Reached")
+        return distanses[i][j]
     if i==j:
-        #print("Reached")
+        print("Reached")
         return 0
     ls=[]
     for p in range(1, n+1):
-        #print(i, p)
+        print(i, p)
         if F[i][p]==1:
-            #print(visited)
+            print(visited)
             if visited[p]==0:
                 
-                #print(i, p)
-                #print(visited)
+                print(i, p)
+                print(visited)
                 visited[p]=-2
                 ans=dfc(p, j)
-                if ans!=-1:
+                if ans>=0:
                     ls.append(ans+1)
-                    #print("Ls-app: ", ls, ans+1)
+                    print("New road: ", ls)
+                    if distanses[i][p]>=0:
+                        t=min(ans, distanses[i][p])
+                    else:
+                        t=ans
+                    distanses[i][p]=distanses[p][i]=t+1
                 visited[p]=0
-            '''else:
-                if visited[p]>0:
-                    if len(ls)>0:
-                        ls[-1]=min(ls[-1], visited[i][p]+1)
-                    else: ls.append(visited[i][p]+1)
-                    visited[i][p]=ls[-1]
-                    print("New road", i, p, j, ls[-1])'''
     else:
         if len(ls)>0:
-            return min(ls)
+            q=min(ls)
+            distanses[i][j]=distanses[j][i]=q
+            print("Renew data: ", q, i, j)
+            for i in distanses:
+                for j in i:
+                    if j>=0:
+                        print('', j, end='')
+                    else:
+                        print(j, end='')
+                print()            
+            return q
         else:
-            #print("Break way", (i, j))
-            return -1
+            print("Break way", (i, j))
+            return -2
 Ans=float("inf")
-for i in pars:
+for w in pars:
     l=0    
     lens.append(0)
     visited=[0]*(n+1)
-    visited[i[0]]=-2
-    #print("Start: ", i)
-    z=dfc(i[0], i[1])
+    visited[w[0]]=-2
+    print("Start: ", w)
+    z=dfc(w[0], w[1])
     if z>0:
         Ans=min(z, Ans)
-    #print("Finish: ", Ans)
+    print("Finish: ", w, z)
     #if lens[-1]==0: lens.pop()
 #print(min(lens))
 #print(lens)
 print(Ans)
-        
-            
-        
+for i in distanses:
+    for j in i:
+        if j>=0:
+            print('', j, end='')
+        else:
+            print(j, end='')
+    print()
+
+'''
+9 8
+5 1 7 0 2 0 0 1 5
+1 9
+1 7
+2 7
+2 6
+2 4
+3 4
+4 5
+6 7
+'''
