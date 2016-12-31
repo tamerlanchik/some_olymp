5b2f7fc9acc44de0424a15aecae329eae8f99b00
Andrey K <andreykochnov@yandex.ru>
2016-12-31 20:19:03 +0300

    Update C.cpp

--- C.cpp
+++ C.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 int main()
 {
-	unsigned long int n, m;
-	unsigned long int x, y;
+	unsigned int n, m;
+	unsigned int x, y;
 	cin >> n >> m;
 	//vector <unsigned int> *a=new vector<unsigned int>;
 	//a->resize(n);
@@ -13,7 +13,7 @@ int main()
 	for (auto i = 0; i < n; i++)
 		cin >> a[i];
 
-	vector<unsigned long int>pars;
+	vector<unsigned int>pars;
 	for (auto i = 0; i < n; i++)
 	{
 		for (auto j = i + 1; j < n; j++)
@@ -31,19 +31,19 @@ int main()
 		F[x - 1][y - 1] = true;
 		F[y - 1][x - 1] = true;
 	}
-	vector<vector<unsigned long int>> dist(n, vector<unsigned long int>(n, -1));
+	vector<vector<unsigned int>> dist(n, vector<unsigned int>(n, -1));
 	for (auto i = 0; i < n; i++)
 		dist[i][i] = 0;
-	unsigned long int i;
+	unsigned int i;
 	for (auto k = 0; k < pars.size(); k += 2)
 	{
 		i = pars[k];
-		queue<unsigned long int> q;
+		queue<unsigned int> q;
 		q.push(i);
 		while (!q.empty())
 		{
-			unsigned long int u = q.front(); q.pop();
-			for (unsigned long int v = 0; v < n; v++)
+			unsigned int u = q.front(); q.pop();
+			for (unsigned int v = 0; v < n; v++)
 				if (F[u][v] == true && dist[i][v] == -1)
 				{
 					dist[i][v] = dist[i][u] + 1;
@@ -51,8 +51,8 @@ int main()
 				}
 		}
 	}
-	unsigned long int ans = 1000000000;
-	unsigned long int aw;
+	unsigned int ans = 1000000000;
+	unsigned int aw;
 	for (auto i = 0; i < pars.size(); i+=2)
 	{
 		aw = dist[pars[i]][pars[i + 1]];
