97c3800b90a14e151eca5e6cea7255d9949390e5
Andrey K <andreykochnov@yandex.ru>
2016-12-31 23:02:04 +0300

    Сжато

--- C.cpp
+++ C.cpp
@@ -9,51 +9,41 @@ int main()
 	for (auto i = 0; i < n; i++)
 		std::cin >> a[i];
 
-	std::vector<unsigned int>pars;
-	for (auto i = 0; i < n; i++)
-	{
-		for (auto j = i + 1; j < n; j++)
-			if ((a[i] * a[j] != 0) && (a[i] != a[j]))
-			{
-				pars.push_back(i);
-				pars.push_back(j);
-			}
-	}
-	delete[] a;
-	unsigned int x, y;
+	unsigned int u;
+	unsigned int ans = 1000000000;
+	unsigned int aw;
+	unsigned int *dist = new unsigned int[n];
 	std::vector<unsigned int> *F = new std::vector<unsigned int>[n];
+	unsigned int x, y;
 	for (auto i = 0; i < m; i++)
 	{
 		std::cin >> x >> y;
 		F[x - 1].push_back(y - 1);
 		F[y - 1].push_back(x - 1);
 	}
-
-
-
-	unsigned int u;
-	unsigned int ans = 1000000000;
-	unsigned int aw;
-	unsigned int *dist = new unsigned int[n];
 	std::queue<unsigned int> q;
-	for (auto k = 0; k < pars.size(); k += 2)
+	for (auto i = 0; i < n; i++)
 	{
-		q.push(pars[k]);
-		for (auto l = 0; l < n; l++)
-			dist[l] = -1;
-		dist[pars[k]] = 0;
-		while (!q.empty())
-		{
-			u = q.front(); q.pop();
-			for (auto v:F[u])
-				if (dist[v] == -1)
+		for (auto j = i + 1; j < n; j++)
+			if ((a[i] * a[j] != 0) && (a[i] != a[j]))
+			{
+				q.push(i);
+				for (auto l = 0; l < n; l++)
+					dist[l] = -1;
+				dist[i] = 0;
+				while (!q.empty())
 				{
-					dist[v] = dist[u] + 1;
-					q.push(v);
+					u = q.front(); q.pop();
+					for (auto v : F[u])
+						if (dist[v] == -1)
+						{
+							dist[v] = dist[u] + 1;
+							q.push(v);
+						}
 				}
-		}
-		aw = dist[pars[k + 1]];
-		if (aw > 0 && aw < ans) ans = aw;
+				aw = dist[j];
+				if (aw > 0 && aw < ans) ans = aw;
+			}
 	}
 
 	std::cout << ans;
